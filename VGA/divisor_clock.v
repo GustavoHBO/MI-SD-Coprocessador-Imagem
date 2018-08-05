dfdbd823f9023be94274f2c84dae8e4db5c71853
victormunduruca <victormunduruca@gmail.com>
2018-08-05 19:01:37 -0300

    colocando versão correta do vga

--- VGA/divisor_clock.v
+++ VGA/divisor_clock.v
@@ -7,24 +7,11 @@ input clk;
 reg clk_in;
 reg contador = 0;
 output reg clk_out;
-<<<<<<< HEAD
-
-
-always @ (posedge clk) begin 
-    if (contador == 1'b1) begin 
-        clk_out <= ~clk_out;
-        contador <= 1'd0;
-    end 
-    else begin 
-        contador <= contador + 1;
-    end
-=======
 
 
 
 always @ (posedge clk) begin 
     clk_out <= ~clk_out;
->>>>>>> ac1a6383334e61969ddfbc91bd057c6723e877ec
 end 
 
 endmodule 
